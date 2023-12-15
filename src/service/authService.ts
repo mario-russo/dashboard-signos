f811769a6a60258493646a17bf65a4998f72fd88
Mario Russo <mario.russo93@hotmail.com>
2023-12-15 18:25:24 -0300

    Fix : remoção do do typo user para o arquivos dos tipos

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,12 +1,10 @@
+import { UserLogin } from '../types'
 import { api } from './config'
 const path = 'auth/login'
 const verifyPath = 'auth/verify'
 
-interface User {
-    email: string,
-    senha: string
-}
-export async function login(user: User) {
+
+export async function login(user: UserLogin) {
     const resposta = await api.post(path, user)
     return (resposta).data
 }
