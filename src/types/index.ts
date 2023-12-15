528680d63bb30aaaa8d25b05479313b1afdebcc5
Mario Russo <mario.russo93@hotmail.com>
2023-12-15 18:24:25 -0300

    Feat : interface dos tipos

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface UserLogin {
+    email: string,
+    senha: string
+}
+export interface Conteudo {
+    conteudo: string,
+    signo: string,
+    referencia: string,
+    idUsuario: number
+
+}
+export interface Usuario {
+    nome: string,
+    id: number
+}
