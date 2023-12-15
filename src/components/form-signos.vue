15996f3966a941d6b60558efe9c582c1e4769791
Mario Russo <mario.russo93@hotmail.com>
2023-12-15 18:29:15 -0300

    Feat : salvando conteudo

--- src/components/form-signos.vue
+++ src/components/form-signos.vue
@@ -1,41 +1,48 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { getAllSignos } from "../service/enumSignos";
+import { Conteudo } from "../types";
+import { salvar } from "../service/conteudoService";
+import { userStore } from "../store/user";
 
-const signo = ref(null)
-const conteudo = ref(null)
-const referencia = ref(null)
+const user = userStore()
+
+const conteudo = reactive<Conteudo>({ conteudo: '', idUsuario: user.getUsuario.id, referencia: '', signo: '' })
 const accept = ref(false)
 const optionSignos = ref();
 
 onMounted(async () => {
   optionSignos.value = await getAllSignos()
 })
+
+async function salvarConteudo() {
+  await salvar(conteudo)
+}
+
 </script>
 <template>
   <div>
     <section class="centro">
-      <q-form class="q-gutter-md">
-        <q-select v-model="signo" :options="optionSignos" color="purple-12" label="Selecione o Signo">
+      <section> class="q-gutter-md">
+        <q-select v-model="conteudo.signo" :options="optionSignos" color="purple-12" label="Selecione o Signo">
           <template v-slot:prepend>
             <q-icon name="event" />
           </template>
         </q-select>
-        <q-input v-model="referencia" label="Referência*" hint="Name and surname" lazy-rules
+        <q-input v-model="conteudo.referencia" label="Referência*" hint="Name and surname" lazy-rules
           :rules="[val => val && val.length > 0 || 'Please type something']" />
 
 
-        <q-input v-model="conteudo" type="textarea" label="Digite o Texto Do Signo" />
+        <q-input v-model="conteudo.conteudo" type="textarea" label="Digite o Texto Do Signo" />
         <q-toggle v-model="accept" label="I accept the license and terms" />
 
         <div>
-          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Submit" type="submit" color="primary" @click="salvarConteudo" />
           <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
         </div>
-      </q-form>
-
+      </section>
     </section>
-
+    {{ conteudo.conteudo.length }}
   </div>
 </template>
 
