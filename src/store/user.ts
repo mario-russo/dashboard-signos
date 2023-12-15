93c48009cf8015d66bb78dbeea4d84d6c3dcc9e8
Mario Russo <mario.russo93@hotmail.com>
2023-12-15 18:22:31 -0300

    Feat : store para usuario

---
+++ src/store/user.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { computed, reactive } from 'vue'
+import { Usuario } from '../types'
+
+// You can name the return value of `defineStore()` anything you want,
+// but it's best to use the name of the store and surround it with `use`
+// and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
+// the first argument is a unique id of the store across your application
+export const userStore = defineStore('user', () => {
+    const user = reactive<Usuario>({ nome: '', id: 0 })
+
+    function setUsuario(usuario: Usuario) {
+        user.id = usuario.id
+        user.nome = usuario.nome
+    }
+    const getUsuario = computed(() => user)
+
+    return { setUsuario, getUsuario }
+})
