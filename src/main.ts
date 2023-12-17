190a224e29fa54292c5505b506bd7d416532e378
Mario Russo <mario.russo93@hotmail.com>
2023-12-17 00:00:24 -0300

    Style : adicionado icones -Importe para os icones

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ import router from './router'
 
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
 
 // Import Quasar css
 import 'quasar/src/css/index.sass'
