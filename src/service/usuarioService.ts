4967d1b27b2923ab2c0ae82b69adfafbb7ddee4b
Mario Russo <mario.russo93@hotmail.com>
2023-12-17 01:49:09 -0300

    Feat : Serviço de usuario - Conexão com api de usuário

---
+++ src/service/usuarioService.ts
@@ -0,0 +1,38 @@
+import { Usuario } from '../types';
+import { api } from './config';
+
+const usuariopath = "usuario"
+
+export async function salvar(usuario: Usuario) {
+    try {
+        const resultado = api.post(usuariopath, usuario)
+        return (await resultado).data
+
+    } catch (error) {
+
+    }
+}
+export async function listaTodosUsuarios() {
+    try {
+        const resultado = api.get(usuariopath)
+        return (await resultado).data
+    } catch (error) {
+
+    }
+}
+export async function autualizarUsuario(usuario: Usuario) {
+    try {
+        const resultado = await api.patch(usuariopath, usuario)
+        return resultado.data
+    } catch (error) {
+
+    }
+}
+export async function deletaUsuario(usuario: Usuario) {
+    try {
+        const resultado = api.delete(usuariopath + usuario.id)
+        return (await resultado).data
+    } catch (error) {
+
+    }
+}
