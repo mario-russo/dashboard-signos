1a457fb04c9a1dce6f053aae89863f50bf33f93e
Mario Russo <mario.russo93@hotmail.com>
2023-12-17 02:02:36 -0300

    Refactor :  mudando interface - mudança de interface usuario para usuarioStore

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,15 +1,15 @@
 import { defineStore } from 'pinia'
 import { computed, reactive } from 'vue'
-import { Usuario } from '../types'
+import { UsuarioStore } from '../types'
 
 // You can name the return value of `defineStore()` anything you want,
 // but it's best to use the name of the store and surround it with `use`
 // and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
 export const userStore = defineStore('user', () => {
-    const user = reactive<Usuario>({ nome: '', id: 0 })
+    const user = reactive<UsuarioStore>({ nome: '', id: 0 })
 
-    function setUsuario(usuario: Usuario) {
+    function setUsuario(usuario: UsuarioStore) {
         user.id = usuario.id
         user.nome = usuario.nome
     }
