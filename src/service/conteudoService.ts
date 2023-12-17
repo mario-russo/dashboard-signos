34955f105c145e90b5e5368dc44b4e351a1e3974
Mario Russo <mario.russo93@hotmail.com>
2023-12-17 00:01:50 -0300

    Feat : LIsta conteudo - Adição de funão para lisatar todos os conteudo.

--- src/service/conteudoService.ts
+++ src/service/conteudoService.ts
@@ -7,3 +7,7 @@ export async function salvar(conteudo: Conteudo) {
     const resultado = await api.post(pathConteudo, conteudo)
     return resultado.data
 }
+export async function listaAllCunteudo() {
+    const resultado = await api.get(pathConteudo)
+    return resultado.data
+}
