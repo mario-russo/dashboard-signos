40dce2763d8e27e81c72ea643b180785fe7f9484
Mario Russo <mario.russo93@hotmail.com>
2023-12-19 00:49:01 -0300

    Feat : Deletar conteudo - Função para deletar conteudo

--- src/service/conteudoService.ts
+++ src/service/conteudoService.ts
@@ -4,10 +4,14 @@ import { api } from './config';
 const pathConteudo = 'conteudo'
 
 export async function salvar(conteudo: Conteudo) {
-    const resultado = await api.post(pathConteudo, conteudo)
-    return resultado.data
+    const resultado = api.post(pathConteudo, conteudo)
+    return (await resultado).data
 }
 export async function listaAllCunteudo() {
-    const resultado = await api.get(pathConteudo)
-    return resultado.data
+    const resultado = api.get(pathConteudo)
+    return (await resultado).data
+}
+export async function deleteConteudo(conteudo: Conteudo) {
+    const resultado = api.delete(pathConteudo, { data: conteudo })
+    return (await resultado).data
 }
